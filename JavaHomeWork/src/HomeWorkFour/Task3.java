4dbeb38e89a898a663cdddb9b424a83e81a14184
Andrey Drozdov <drozdov_andrey@list.ru>
2022-11-10 01:18:36 +0300

    added task 3

--- JavaHomeWork/src/HomeWorkFour/Task3.java
+++ JavaHomeWork/src/HomeWorkFour/Task3.java
@@ -11,5 +11,85 @@ package HomeWorkFour;
 *3
 9
  */
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Task3 {
+    public static int Calc(int a, int b, String sign) {
+        int res = 0;
+        switch (sign) {
+            case ("+"):
+                res = a + b;
+                break;
+
+            case ("-"):
+                res = a - b;
+                break;
+
+            case ("*"):
+                res = a * b;
+                break;
+
+            case ("/"):
+                res = a / b;
+                break;
+
+            default:
+                break;
+
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LinkedList<Integer> lk = new LinkedList<>();
+        System.out.println("Добро пожаловать в калькулятор");
+
+        System.out.println("Введите число:");
+        int a = sc.nextInt();
+        lk.addFirst(a);
+        System.out.println("Выберете операцию: -,+,/ или *:");
+        String sign = sc.next();
+        System.out.println("Введите число: ");
+        int b = sc.nextInt();
+        int res = Calc(a, b, sign);
+        lk.addFirst(res);
+        System.out.printf("Результат = %d\n", res);
+
+        while (true){
+            System.out.println("хотите продолжить - yes, выйти - no, отменить последнюю операцию - cancel");
+            String choice = sc.nextLine();
+            if (choice.equals("yes")) {
+                a = lk.getFirst();
+                System.out.println(a);
+                System.out.println("Выберете операцию: -,+,/ или *:");
+                sign = sc.next();
+                System.out.println("Введите число:");
+                b = sc.nextInt();
+                res = Calc(a, b, sign);
+                lk.addFirst(res);
+                System.out.printf("Результат = %d\n", res);
+            }
+            if (choice.equals("cancel")){
+                lk.removeFirst();
+                a = lk.getFirst();
+                System.out.println(a);
+                System.out.println("Выберете операцию: -,+,/ или *:");
+                sign = sc.next();
+                System.out.println("Введите число:");
+                b = sc.nextInt();
+                res = Calc(a, b, sign);
+                lk.addFirst(res);
+                System.out.printf("Результат = %d\n", res);
+            }
+            if (choice.equals("no")) break;
+
+        }
+
+
+    }
+
+
 }
+
