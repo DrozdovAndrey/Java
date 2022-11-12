ea81c6a1f873579900b34100a6a4848ddd14544f
Andrey Drozdov <drozdov_andrey@list.ru>
2022-11-12 18:35:31 +0300

    added task2

--- JavaHomeWork/src/HomeWorkFive/Task2.java
+++ JavaHomeWork/src/HomeWorkFive/Task2.java
@@ -1,2 +1,72 @@
-package HomeWorkFive;public class Task2 {
+package HomeWorkFive;
+
+import java.util.*;
+import java.util.stream.Stream;
+
+/*
+Пусть дан список сотрудников:
+Иван Иванов
+Светлана Петрова
+Кристина Белова
+Анна Мусина
+Анна Крутова
+Иван Юрин
+Петр Лыков
+Павел Чернов
+Петр Чернышов
+Мария Федорова
+Марина Светлова
+Мария Савина
+Мария Рыкова
+Марина Лугова
+Анна Владимирова
+Иван Мечников
+Петр Петин
+Иван Ежов
+
+Написать программу, которая найдет и выведет повторяющиеся
+ имена с количеством повторений.
+ Отсортировать по убыванию популярности Имени
+ */
+public class Task2 {
+    public static void main(String[] args) {
+        String[] array = new String[] {
+                "Иван Иванов",
+                "Светлана Петрова",
+                "Кристина Белова",
+                "Анна Мусина",
+                "Анна Крутова",
+                "Иван Юрин",
+                "Петр Лыков",
+                "Павел Чернов",
+                "Петр Чернышов",
+                "Мария Федорова",
+                "Марина Светлова",
+                "Мария Савина",
+                "Мария Рыкова",
+                "Марина Лугова",
+                "Анна Владимирова",
+                "Иван Мечников",
+                "Петр Петин",
+                "Иван Ежов",
+        };
+        sortByName(array);
+
+    }
+    public static void sortByName(String [] array){
+        Map<String, Integer> countName = new HashMap<>();
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i].split(" ")[0].equals(array[j].split(" ")[0])){
+                    count++;
+                }
+            }
+            countName.put(array[i].split(" ")[0],count);
+            count=0;
+        }
+        countName.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).forEach(System.out::println);
+
+    }
 }
