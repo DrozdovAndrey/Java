af559702eed27f905dfacbafbf60fcb0bd53dd35
Andrey Drozdov <drozdov_andrey@list.ru>
2022-11-16 17:13:40 +0300

    task12

--- JavaSeminar/src/SeminarSix/Task1.java
+++ JavaSeminar/src/SeminarSix/Task1.java
@@ -1,2 +1,20 @@
-package SeminarSix;public class Task1 {
+package SeminarSix;
+
+import java.util.*;
+
+/*
+1. —оздайте HashSet, заполните его следующими числами: {1, 2, 3, 2, 4, 5, 6, 3}. –аспечатайте содержимое данного множества.
+2. —оздайте LinkedHashSet, заполните его следующими числами: {1, 2, 3, 2, 4, 5, 6, 3}. –аспечатайте содержимое данного множества.
+3. —оздайте TreeSet, заполните его следующими числами: {1, 2, 3, 2, 4, 5, 6, 3}. –аспечатайте содержимое данного множества.
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        Set <Integer> set1 = new HashSet<>(Arrays.asList(100,4,5,1,2,5,3,9));
+        System.out.println(set1);
+        Set <Integer> set2 = new LinkedHashSet<>(Arrays.asList(100,4,5,1,2,5,3,9));
+        System.out.println(set2);
+        Set <Integer> set3 = new TreeSet<>(Arrays.asList(100,4,5,1,2,5,3,9));
+        System.out.println(set3);
+    }
+
 }
--- JavaSeminar/src/SeminarSix/Task2.java
+++ JavaSeminar/src/SeminarSix/Task2.java
@@ -1,2 +1,38 @@
-package SeminarSix;public class Task2 {
+package SeminarSix;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+/*
+1. Напишите метод, который заполнит массив из 1000 элементов случайными цифрами от 0 до 24.
+2. Создайте метод, в который передайте заполненный выше массив и
+с помощью Set вычислите процент уникальных значений в данном массиве и верните его в виде числа с плавающей запятой.
+Для вычисления процента используйте формулу:
+процент уникальных чисел = количество уникальных чисел * 100 / общее количество чисел в массиве.
+ */
+public class Task2 {
+    public static Integer[] fillRandom () {
+        Random rnd = new Random();
+        Integer[] arr = new Integer[100];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rnd.nextInt(0, 25);
+        }
+        return arr;
+    }
+    public static double percentUni(Integer[] arr){
+        Set<Integer> set = new HashSet<>(Arrays.asList(arr));
+        return (set.size()*100)/ arr.length;
+    }
+    public static void main(String[] args) {
+        Integer [] arr = fillRandom();
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        double count = percentUni(arr);
+        System.out.println(count);
+
+    }
 }
