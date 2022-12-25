254623ca31c1831f6baa51232eef602b75d3505c
Andrey Drozdov <drozdov_andrey@list.ru>
2022-12-25 12:52:57 +0300

    changed tsk2

--- JavaHomeWork/src/ExceptionHomeWorkTwo/Task2.java
+++ JavaHomeWork/src/ExceptionHomeWorkTwo/Task2.java
@@ -6,9 +6,12 @@ public class Task2 {
     public static void main(String[] args) {
         int d = getInt();
         int[] intArray = {1,2,3,4,5,6,7,8,9,10};
+        int arrIndex = getInt();
         if (d!=0) {
-            double catchedRes1 = (double) intArray[8] / d;
-            System.out.println("catchedRes1 = " + catchedRes1);
+            if (arrIndex> -1 && arrIndex < intArray.length) {
+                double catchedRes1 = (double) intArray[8] / d;
+                System.out.println("catchedRes1 = " + catchedRes1);
+            }else System.out.println("out of array length");
         }else System.out.println("You can't divide by zero");
     }
     public static int getInt(){
