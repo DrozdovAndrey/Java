405748de2b4b2d7f82cb6a16c3c8f5ef2dfc810b
Andrey Drozdov <drozdov_andrey@list.ru>
2022-12-25 13:02:22 +0300

    changed tsk3

--- JavaHomeWork/src/ExceptionHomeWorkTwo/Task3.java
+++ JavaHomeWork/src/ExceptionHomeWorkTwo/Task3.java
@@ -2,25 +2,34 @@ package ExceptionHomeWorkTwo;
 
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) throws Exception {
-        try {
-            int a = 90;
-            int b = 3;
+        int a = getInt();
+        int b = getInt();
+        if (b!=0){
             System.out.println(a / b);
-            printSum(5, 234);
-            int[] abc = { 1, 2 };
-            abc[1] = 9;
-            System.out.println(Arrays.toString(abc));
-        } catch (ArithmeticException | NullPointerException ex) {
-            System.out.println("Exception: "+ex.getMessage());
-        } catch (IndexOutOfBoundsException ex) {
-            System.out.println("ћассив выходит за пределы своего размера!");
         }
+        printSum(5, 234);
+        int[] abc = { 1, 2 };
+        int arrIndex = getInt();
+        if (arrIndex> -1 && arrIndex < abc.length){
+            abc[arrIndex] = 9;
+            System.out.println(Arrays.toString(abc));
+        } else System.out.println("out of array length");
+
+
     }
     public static void printSum(Integer a, Integer b){
-        System.out.println(a + b);
+        if (a == null || b == null) System.out.println("null");
+         else System.out.println(a+b);
+    }
+    public static int getInt(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input integer number");
+        if (!sc.hasNextInt()) throw new RuntimeException("You can input only Integer number");
+        return sc.nextInt();
     }
 
 }
